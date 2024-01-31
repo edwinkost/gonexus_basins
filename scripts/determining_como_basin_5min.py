1e2dfdd5b3ce33d727bb83c8434c06259383b97c
Edwin Sutanudjaja <edwinkost@UU105378>
2024-01-31 16:06:51 +0100

    create the script to make the basin clone map

--- scripts/determining_como_basin_5min.py
+++ scripts/determining_como_basin_5min.py
@@ -4,7 +4,6 @@
 import os
 import sys
 import shutil
-import subprocess
 
 import pcraster as pcr
 
@@ -33,17 +32,13 @@ pcr.setclone(clone_map)
 
 # convert the original input file to 5 arcmin resolution at the global extent, and read it
 cmd = 'gdalwarp -t_srs "+proj=longlat +ellps=WGS84" -tr 0.08333333333333333333333333333333333333333333333333333 0.08333333333333333333333333333333333333333333333333333 -te -180 -90 180 90 ' + original_input_file + " " + os.path.basename(original_input_file) + ".tif"
-print(cmd); subprocess.run(cmd.split(), check = True)
-
-
+print(cmd); os.system(cmd)
 # - convert to map
 cmd = 'gdal_translate -of PCRaster ' + os.path.basename(original_input_file) + ".tif " + os.path.basename(original_input_file) + ".tif.map"
-print(cmd); subprocess.run(cmd.split(), check = True)
+print(cmd); os.system(cmd)
 # - make sure that its mapattr consistent with clone_map
 cmd = 'mapattr -c ' + clone_map + " " + os.path.basename(original_input_file) + ".tif.map"
-print(cmd); subprocess.run(cmd.split(), check = True)
+print(cmd); os.system(cmd)
 # - read the map
 basin_5min_original = pcr.readmap(os.path.basename(original_input_file) + ".tif.map")
 pcr.aguila(basin_5min_original) 
-
-
