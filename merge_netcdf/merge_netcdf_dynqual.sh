0ab1ce6f92576a1fbed6ec177751adbd6660f4d8
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2024-03-26 16:00:13 +0100

    merge netcdf for dynqual

---
+++ merge_netcdf/merge_netcdf_dynqual.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -x
+
+python merge_netcdf_general_selected_dynqual.py /scratch/depfg/graha010/DYNQUAL_GLOBAL_OUTPUT_ONLINE/ /scratch/depfg/sutan101/dynqual_output_from_duncan/test/ outDailyTotNC 1980-01-01 2019-12-31 discharge
+NETCDF4 True 8 dynqual_selected all_lats &
+
+python merge_netcdf_general_selected_dynqual.py /scratch/depfg/graha010/DYNQUAL_GLOBAL_OUTPUT_ONLINE/ /scratch/depfg/sutan101/dynqual_output_from_duncan/test/ outDailyTotNC 1980-01-01 2019-12-31 waterTemp
+NETCDF4 True 8 dynqual_selected all_lats &
+
+wait
+
+set +x
+
+
+
