78c8447aa8fc74a2fb7458e1b32ea43ae7473219
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2024-03-29 00:12:25 +0100

    processing danube and senegal

--- scripts/cropping_senegal_basin_5min.py
+++ scripts/cropping_senegal_basin_5min.py
@@ -86,11 +86,11 @@ basin_5min_pcrglobwb = pcr.catchment(ldd_map, basin_5min_pcrglobwb)
 basin_5min_pcrglobwb = pcr.ifthen(basin_5min_pcrglobwb, basin_5min_pcrglobwb)
 # ~ pcr.aguila(basin_5min_pcrglobwb)
 
-# - include inland sinks
-pits_on_basin_5min_original        = pcr.ifthen(basin_5min_original, pcr.pit(ldd_map))
-catchments_flowing_to_pits         = pcr.catchment(ldd_map, pits_on_basin_5min_original)
-catchments_flowing_to_pits_boolean = pcr.ifthen(pcr.scalar(catchments_flowing_to_pits) > 0, pcr.boolean(1.0))
-basin_5min_pcrglobwb = pcr.cover(basin_5min_pcrglobwb, catchments_flowing_to_pits_boolean)
+# ~ # - include inland sinks
+# ~ pits_on_basin_5min_original        = pcr.ifthen(basin_5min_original, pcr.pit(ldd_map))
+# ~ catchments_flowing_to_pits         = pcr.catchment(ldd_map, pits_on_basin_5min_original)
+# ~ catchments_flowing_to_pits_boolean = pcr.ifthen(pcr.scalar(catchments_flowing_to_pits) > 0, pcr.boolean(1.0))
+# ~ basin_5min_pcrglobwb = pcr.cover(basin_5min_pcrglobwb, catchments_flowing_to_pits_boolean)
 # ~ pcr.aguila(basin_5min_pcrglobwb)
 
 # - save the file as a nominal map
